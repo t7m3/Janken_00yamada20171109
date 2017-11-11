dfb11c79ed31a9bfda002b9ddff0848341f03f26
NJC-T1 <toda7.mk3@gmail.com>
2017-11-11 11:50:56 +0900

    2017-11-11の４回目コミット

--- app/src/main/java/com/example/njc_t1/janken_00yamada20171109/ResultActivity.java
+++ app/src/main/java/com/example/njc_t1/janken_00yamada20171109/ResultActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
     //グー、チョキ、パーの設定
@@ -21,17 +22,23 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int id = intent.getIntExtra("MY_HAND",0);
 
-        //idから、前のアクティビティで選んだ手を表示する
-        ImageView myHandImageView = (ImageView) findViewById(R.id.my_hand_image);
+        //自分の手を格納する変数の宣言
+        int myHand = 0;
+
+        //idから、前のアクティビティで選んだ手を表示して、変数の格納しておく
+        ImageView myHandImageView = (ImageView) findViewById(R.id.my_hand_image);     //ImageViewの参照値を取得する。findViewById()メソッドで。
         switch (id) {
             case R.id.gu:
-                myHandImageView.setImageResource(R.drawable.gu);
+                myHandImageView.setImageResource(R.drawable.gu);     //グーの画像を表示する
+                myHand = JANKEN_GU;     //グーを保存
                 break;
             case R.id.choki:
-                myHandImageView.setImageResource(R.drawable.choki);
+                myHandImageView.setImageResource(R.drawable.choki);     //チョキの画像を表示する
+                myHand = JANKEN_CHOKI;     //チョキを保存
                 break;
             case R.id.pa:
-                myHandImageView.setImageResource(R.drawable.pa);
+                myHandImageView.setImageResource(R.drawable.pa);     //パーの画像を表示する
+                myHand = JANKEN_PA;     //パーを保存
                 break;
             default:
                 break;
@@ -54,6 +61,28 @@ public class ResultActivity extends AppCompatActivity {
                 comHandImageView.setImageResource(R.drawable.com_pa);     //パーの画像を表示する
                 break;
         }
+
+
+        // 勝敗を判定する
+        int gameResult = (comHand - myHand + 3) % 3;
+
+        //勝敗を表示する
+        TextView resultLabel = (TextView) findViewById(R.id.result_label);
+        switch (gameResult) {
+            case 0:
+                // あいこの場合
+                resultLabel.setText(R.string.result_draw);
+                break;
+            case 1:
+                // 勝った場合
+                resultLabel.setText(R.string.result_win);
+                break;
+            case 2:
+                // 負けた場合
+                resultLabel.setText(R.string.result_lose);
+                break;
+        }
+
     }
 
     //前の画面に戻る
