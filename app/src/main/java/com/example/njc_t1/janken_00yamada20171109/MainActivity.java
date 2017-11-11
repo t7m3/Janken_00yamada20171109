ddd349d70364a28551e96334317705fce9f28bb9
NJC-T1 <toda7.mk3@gmail.com>
2017-11-11 12:15:11 +0900

    2017-11-11の５回目コミット

--- app/src/main/java/com/example/njc_t1/janken_00yamada20171109/MainActivity.java
+++ app/src/main/java/com/example/njc_t1/janken_00yamada20171109/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.njc_t1.janken_00yamada20171109;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 起動時に、共有ぷリファレンスのデータをクリアする
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
     }
 
+    //他のアクティビティに遷移する
     public void onJankenButtonTapped(View view){
         Intent intent = new Intent(this, ResultActivity.class);
         intent.putExtra("MY_HAND",view.getId());     //インテントに追加情報を格納する。
--- app/src/main/java/com/example/njc_t1/janken_00yamada20171109/ResultActivity.java
+++ app/src/main/java/com/example/njc_t1/janken_00yamada20171109/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.example.njc_t1.janken_00yamada20171109;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -45,7 +47,10 @@ public class ResultActivity extends AppCompatActivity {
         }
 
         // コンピュータの手を決める（乱数）
-        int comHand =(int) (Math.random() * 3);
+       // int comHand =(int) (Math.random() * 3);
+
+        //コンピュータの手を決める（じゃんけんロジックのメソッドで）
+        int comHand = getHand();
 
         //コンピュータの手を表示する。
         ImageView comHandImageView =
@@ -83,10 +88,78 @@ public class ResultActivity extends AppCompatActivity {
                 break;
         }
 
+        // じゃんけんの結果を保存する
+        saveData(myHand, comHand, gameResult);     //共有ぷリファレンスでの保存に伴い、追加したもの
+
     }
 
     //前の画面に戻る
     public void onBackButtonTapped(View view){
         finish();
+    }
+
+
+
+    //前回のデータを読み込み、今回の情報をデータに反映する
+    //データは共有ぷリファレンスで保存する
+    private void saveData(int myHand, int comHand, int gameResult) {
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+
+        int gameCount = pref.getInt("GAME_COUNT", 0);
+        int winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0);
+        int lastComHand = pref.getInt("LAST_COM_HAND", 0);
+        int lastGameResult = pref.getInt("GAME_RESULT", -1);
+
+        editor.putInt("GAME_COUNT", gameCount + 1);
+
+        if (lastGameResult == 2 && gameResult == 2) {
+            // コンピュータが連勝した場合
+            editor.putInt("WINNING_STREAK_COUNT", winningStreakCount + 1);
+        } else {
+            editor.putInt("WINNING_STREAK_COUNT", 0);
+        }
+        editor.putInt("LAST_MY_HAND", myHand);
+        editor.putInt("LAST_COM_HAND", comHand);
+        editor.putInt("BEFORE_LAST_COM_HAND", lastComHand);
+        editor.putInt("GAME_RESULT", gameResult);
+        editor.commit();
+    }
+
+    //じゃんけんロジック
+    private int getHand() {
+        int hand = (int) (Math.random() * 3);
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        int gameCount = pref.getInt("GAME_COUNT", 0);
+        int winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0);
+        int lastMyHand = pref.getInt("LAST_MY_HAND", 0);
+        int lastComHand = pref.getInt("LAST_COM_HAND", 0);
+        int beforeLastComHand = pref.getInt("BEFORE_LAST_COM_HAND", 0);
+        int gameResult = pref.getInt("GAME_RESULT", -1);
+
+        if (gameCount == 1) {
+
+            if (gameResult == 2) {
+                // 前回の勝負が1回目で、コンピュータが勝った場合、
+                // コンピュータは次に出す手を変える
+                while (lastComHand == hand) {
+                    hand = (int) (Math.random() * 3);
+                }
+            } else if (gameResult == 1) {
+                // 前回の勝負が１回目で、コンピュータが負けた場合
+                // 相手の出した手に勝つ手を出す
+                hand = (lastMyHand - 1 + 3) % 3;
+            }
+        } else if (winningStreakCount > 0) {
+            if (beforeLastComHand == lastComHand) {
+                // 同じ手で連勝した場合は手を変える
+                while (lastComHand == hand) {
+                    hand = (int) (Math.random() * 3);
+                }
+            }
+        }
+        return hand;
     }
 }
